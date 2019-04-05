4284b2fcef6ea78eca363578bf7842001383ab59
Kiana Mills <kmills@at-o2.21-school.ru>
2019-04-05 06:00:01 +0300

    draw line -norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 04:04:28 by kmills            #+#    #+#             */
-/*   Updated: 2019/04/05 05:38:52 by kmills           ###   ########.fr       */
+/*   Updated: 2019/04/05 05:59:06 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,57 @@ int		deal_mouse(int mouse, void *param)
 	return (0);
 }
 
+int		*makekoor4(int x1, int y1, int x2, int y2)
+{
+	int		*ko;
+
+	ko = (int *)malloc(sizeof(int) * 4);
+	ko[0] = x1;
+	ko[1] = y1;
+	ko[2] = x2;
+	ko[3] = y2;
+	return (ko);
+}
+
+void	draw_line(void *mlx_ptr, void *win_ptr, int *ko)
+{
+    const int deltaX = abs(ko[2] - ko[0]);
+    const int deltaY = abs(ko[3] - ko[1]);
+    const int signX = ko[0] < ko[2] ? 1 : -1;
+    const int signY = ko[1] < ko[3] ? 1 : -1;
+	int		error = deltaX - deltaY;
+
+	mlx_pixel_put(mlx_ptr, win_ptr, ko[2], ko[3], 0xFFFFFF);
+    while(ko[0] != ko[2] || ko[1] != ko[3]) 
+   {
+        mlx_pixel_put(mlx_ptr, win_ptr, ko[0], ko[1], 0xFFFFFF);
+        const int error2 = error * 2;
+        //
+        if(error2 > -deltaY) 
+        {
+            error -= deltaY;
+            ko[0] += signX;
+        }
+        if(error2 < deltaX) 
+        {
+            error += deltaX;
+            ko[1] += signY;
+        }
+    }
+}
+
 int		main()
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	int		x;
-	int		y;
+	int		*ko;
 
-	x = 0;
-	y = 0;
+	ko = makekoor4(50 ,50 ,200 ,200);
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 800, 600, "test");
-	while (x < 500)
-	{
-		mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFFFFFF);
-		x++;
-		y = y + 3;
-	}
+
+	draw_line(mlx_ptr, win_ptr, ko);
+
 	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	mlx_mouse_hook(win_ptr, deal_mouse, (void *)0);
 	mlx_loop(mlx_ptr);
